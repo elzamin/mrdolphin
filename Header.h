9b1d45a898e90a636df18b8f8a2fb2c3ad5510dd
elzamin <elzamin006@mail.ru>
2016-10-09 19:55:48 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,1 +1,21 @@
-
+class System
+{
+private:
+	float proc_freq;			//Processor frequence [MHz]
+	int proc_cpu;				  //Central Processing Unit[%]
+	int video_gpu;				//Graphics Processing Unit[%]
+	float video_temp;			//Temperatures['c]
+	float ram_u;				  //RAM utilization[MB]
+	int ram_u2;				   	//RAM utilization[%]
+	float hdd_u;			  	//Hard Disc Drive[GB]
+	float hdd_temp;				//Temperatures['c]
+	float hdd_s_record; 	//Speed of record
+	float hdd_s_read;			//Speed of reading
+	float psu_v;				  //Power Supply Unit voltage[V]
+	float psu_c;				  //Capacity[mWh]
+	int fan;					    //Fan speed[%]
+public:
+	System(){};
+	
+	~System(){};
+};
