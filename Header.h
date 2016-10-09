1ae7e2b24def294550fe7737c913b985c455cf6d
elzamin <elzamin006@mail.ru>
2016-10-09 19:56:25 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -6,14 +6,14 @@ private:
 	int video_gpu;				//Graphics Processing Unit[%]
 	float video_temp;			//Temperatures['c]
 	float ram_u;				//RAM utilization[MB]
-	int ram_u2;					//RAM utilization[%]
+	int ram_u2;				//RAM utilization[%]
 	float hdd_u;				//Hard Disc Drive[GB]
 	float hdd_temp;				//Temperatures['c]
 	float hdd_s_record;			//Speed of record
 	float hdd_s_read;			//Speed of reading
 	float psu_v;				//Power Supply Unit voltage[V]
 	float psu_c;				//Capacity[mWh]
-	int fan;					//Fan speed[%]
+	int fan;				//Fan speed[%]
 public:
 	System(){};
 	
