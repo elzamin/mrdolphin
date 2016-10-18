6304d344783a2023b24faa34999ef28e16218586
elzamin <elzamin006@mail.ru>
2016-10-19 00:26:12 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,21 +1,101 @@
-class System
+class R
 {
 private:
-	float proc_freq;			//Processor frequence [MHz]
-	int proc_cpu;				//Central Processing Unit[%]
-	int video_gpu;				//Graphics Processing Unit[%]
-	float video_temp;			//Temperatures['c]
-	float ram_u;				//RAM utilization[MB]
-	int ram_u2;				//RAM utilization[%]
-	float hdd_u;				//Hard Disc Drive[GB]
-	float hdd_temp;				//Temperatures['c]
-	float hdd_s_record;			//Speed of record
-	float hdd_s_read;			//Speed of reading
-	float psu_v;				//Power Supply Unit voltage[V]
-	float psu_c;				//Capacity[mWh]
-	int fan;				//Fan speed[%]
+	CPU _cpu;
+	GPU _gpu;
+	RAM _ram;
+	HDD _hdd;
+	PSU _psu;
+	FAN _fan;
 public:
-	System(){};
-	
-	~System(){};
+	R(){};
+
+	~R(){};
+};
+
+struct CPU
+{
+	float _freq;			//Processor frequence [MHz]
+	float _cpu;				//Central Processing Unit[%]
+};
+
+struct GPU
+{
+	float _gpu;				//Graphics Processing Unit[%]
+	float _temp;			//Temperatures['c]
+};
+
+struct RAM
+{
+	float _u;				//RAM utilization[MB]
+	int _u2;				//RAM utilization[%]
+};
+
+struct HDD
+{
+	float _u;				//Hard Disc Drive[GB]
+	float _temp;			//Temperatures['c]
+	float _s_record;		//Speed of record
+	float _s_read;			//Speed of reading
+};
+
+struct PSU
+{
+	float _v;				//Power Supply Unit, voltage[V]
+	float _c;				//Capacity[mWh]
+};
+
+struct FAN
+{
+	float _fan;				//Fan speed[%]
+};
+
+class OffP					//Monitoring offline processor
+{
+private:
+
+public:
+	OffP(){};
+
+	~OffP(){};
+};
+
+struct offp					//One offline processor
+{
+	float _cp;				//    utilization[%]
+	float _u1;				//RAM utilization[MB]
+	float _u2;				//RAM utilization[%]
+	string _info;			//Description(information)
+	string _ver;			//Varified signer
+};
+
+class OnP					//Monitoring offline proc
+{
+private:
+	float f;
+public:
+	OnP(){};
+
+	~OnP(){};
+};
+
+class H						//History
+{
+private:
+
+public:
+	H(){};
+
+	~H(){};
+};
+
+
+class S						//Statistic
+{
+private:
+
+public:
+	S(){};
+
+	~S(){};
 };
